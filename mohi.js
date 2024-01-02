4c71a96d1edc34df92b427beb3a783ab2603c4df
mohirathore <rathoremohi34@gmail.com>
2024-01-02 15:32:56 +0530

    added more console

--- mohi.js
+++ mohi.js
@@ -5,3 +5,8 @@ console.log("Hi! My name is mohi rathore");
 console.log("Hi! My name is mohi rathore");
 console.log("Hi! My name is mohi rathore");
 console.log("Hi! My name is mohi rathore");
+console.log("Hi! My name is mohi rathore");
+console.log("Hi! My name is mohi rathore");
+console.log("Hi! My name is mohi rathore");
+console.log("Hi! My name is mohi rathore");
+console.log("Hi! My name is mohi rathore");
